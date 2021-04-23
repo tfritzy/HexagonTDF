8fb2eec6c9962f25b6ff0877186b3012ce34a7d3
Tyler Fritz <tfritzy@gmail.com>
2021-04-22 20:37:13 -0700

    Made spark tower a lightning tower

--- Assets/Scripts/Buildings/SparkTower.cs
+++ Assets/Scripts/Buildings/SparkTower.cs
@@ -18,13 +18,48 @@ public class SparkTower : AttackTower
         {ResourceType.Stone, .3f},
         {ResourceType.Food, .1f},
     };
+    private const float DIST_BETWEEN_LIGHTNING_SEGMENTS = .5f;
+    private LineRenderer lr;
+
+    protected override void Setup()
+    {
+        base.Setup();
+        lr = this.GetComponent<LineRenderer>();
+        lr.enabled = false;
+    }
 
     protected override void Attack()
     {
+        List<Vector3> points = new List<Vector3>();
+        Vector3 difference = Target.transform.position - projectileStartPosition;
+        int numPoints = (int)(difference.magnitude / DIST_BETWEEN_LIGHTNING_SEGMENTS);
+        lr.enabled = true;
+        difference = difference.normalized;
+        lr.positionCount = numPoints;
+        for (int i = 0; i < numPoints; i++)
+        {
+            lr.SetPosition(i, projectileStartPosition + (difference * i * DIST_BETWEEN_LIGHTNING_SEGMENTS + Random.insideUnitSphere * DIST_BETWEEN_LIGHTNING_SEGMENTS));
+        }
+
         Target.TakeDamage(Damage);
         GameObject projectile = Instantiate(
                 Prefabs.Projectiles[Type],
                 Target.GetComponent<Collider>().bounds.center,
                 new Quaternion());
+    }
+
+    protected override void UpdateLoop()
+    {
+        base.UpdateLoop();
+
+        if (Time.time > lastAttackTime + .075f)
+        {
+            lr.enabled = false;
+        }
+    }
+
+    private void SetLineRendererEnabledStatus()
+    {
+
     }
 }
