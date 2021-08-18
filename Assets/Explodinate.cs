80c8a7b04fd0c641bf3e607b9c18fd7ae2e03980
Tyler Fritz <tfritzy@gmail.com>
2021-08-17 22:44:58 -0700

    Added explosion to tower

---
+++ Assets/Explodinate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explodinate : MonoBehaviour
+{
+    private Collider[] parts;
+
+    void Start()
+    {
+        parts = this.GetComponentsInChildren<Collider>();
+
+        foreach (Collider part in parts)
+        {
+            Rigidbody rigidbody = part.GetComponent<Rigidbody>();
+            rigidbody.velocity = new Vector3(Random.Range(-3, 3), Random.Range(3, 6), Random.Range(-3, 3));
+            rigidbody.useGravity = true;
+            rigidbody.angularVelocity = Random.insideUnitSphere * Random.Range(10, 30);
+        }
+    }
+}
